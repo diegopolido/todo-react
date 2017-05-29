81be5d62020fb161146ab7ea152544adda2f83b1
Diego Polido Santana <diego@polidosantana.com.br>
2017-05-29 02:48:00 -0300

    Todo app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,82 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {  } from 'react';
 import './App.css';
+window.id = 0;
+const TodoForm = ({addTodo}) => {
+  let input;
 
-class App extends Component {
-  render() {
+  return (
+    <div>
+      <input ref={node => {
+        input = node;
+      }} />
+      <button onClick={() => {
+        addTodo(input.value);
+        input.value = '';
+      }}>
+        +
+      </button>
+    </div>
+  );
+};
+
+const Todo = ({todo, remove}) => {
+  return (<li  onClick={() => {remove(todo.id)}}>{todo.text}</li>);
+}
+
+const TodoList = ({todos, remove}) => {
+  const todoNode = todos.map((todo) => {
+    return (<Todo todo={todo} key={todo.id} remove={remove}/>)
+  });
+  return (<ul>{todoNode}</ul>);
+}
+
+const Title = ({todoCount}) => {
+  return (
+    <div>
+       <div>
+          <h1>to-do ({todoCount})</h1>
+       </div>
+    </div>
+  );
+}
+
+window.id = 0;
+class TodoApp extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      data: [{text: "testtt", id: window.id++}]
+    }
+  }
+  addTodo(val){
+    if(val) {
+      const todo = {text: val, id: window.id++}
+      this.state.data.push(todo);
+      this.setState({data: this.state.data});
+    }
+  }
+  handleRemove(id){
+    const remainder = this.state.data.filter((todo) => {
+      if(todo.id !== id) {
+        return todo;
+      }
+      return null;
+    });
+    this.setState({data: remainder});
+  }
+
+  render(){
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Title todoCount={this.state.data.length}/>
+        <TodoForm addTodo={this.addTodo.bind(this)}/>
+        <TodoList
+          todos={this.state.data}
+          remove={this.handleRemove.bind(this)}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default TodoApp;
